bdbed9dc84118046e0bf89cd55b22528ccd2b1f2
VenusakaVXT <voxuantuan33@gmail.com>
2023-09-10 18:07:58 +0700

    Change title

--- src/Route.js
+++ src/Route.js
@@ -3,14 +3,19 @@ import { useEffect, useState } from 'react'
 const tabs = ['Introduction', 'Frontend', 'Backend', 'Fullstack']
 
 function Route() {
-    const [type, setType] = useState('Introduction')
+    const [type, setType] = useState(tabs[0])
+    const [title, setTitle] = useState(tabs[0])
     const [items, setItems] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:3001/${type}`)
             .then((res) => res.json())
             .then((item) => setItems(item))
-    }, [type])
+
+        // Change title
+        document.title = `Route Tab | ${title}`
+        console.trace('render')
+    }, [type, title])
 
     return (
         <div className='App-container'>
@@ -19,7 +24,10 @@ function Route() {
                     tabs.map((tab) =>
                         <div className='App-tab-item' 
                             key={tab} 
-                            onClick={() => setType(tab)}
+                            onClick={() => {
+                                setType(tab)
+                                setTitle(tab)
+                            }}
                             style={
                                 type === tab ? { color: '#61dafb'} : {}
                             }
